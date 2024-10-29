6b6811a5baf78ee49db17e9775db4ae62fca3ccf
vivekcorp <95239073+vivekcorp@users.noreply.github.com>
2024-10-29 09:12:16 +0530

    Add files via upload

---
+++ Maintenance.aspx.cs
@@ -0,0 +1,9 @@
+using System;
+
+public partial class Maintenance : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // Optional: Add any additional maintenance mode logic here, if needed.
+    }
+}
